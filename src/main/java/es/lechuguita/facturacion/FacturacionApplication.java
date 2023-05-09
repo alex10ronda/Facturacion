ea85bb0a7f6fdf74fa0ac111bd852a7f8e09c9c2
alex10ronda <alex10_ronda@hotmail.com>
2023-05-09 16:58:26 +0200

    Incluir logs

--- src/main/java/es/lechuguita/facturacion/FacturacionApplication.java
+++ src/main/java/es/lechuguita/facturacion/FacturacionApplication.java
@@ -1,6 +1,10 @@
 package es.lechuguita.facturacion;
 
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
@@ -10,26 +14,36 @@ import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class FacturacionApplication extends Application{
-
+	
+	private Logger log = LogManager.getLogger(FacturacionApplication.class);
+	
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
-		Screen screen = Screen.getPrimary();
-		Rectangle2D bounds = screen.getVisualBounds();
-
-		primaryStage.setX(bounds.getMinX());
-		primaryStage.setY(bounds.getMinY());
-		primaryStage.setWidth(bounds.getWidth());
-		primaryStage.setHeight(bounds.getHeight());
+		log.info("Se arranca la aplicacion");
 		
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Main.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
+		try {
+			
+			Screen screen = Screen.getPrimary();
+			Rectangle2D bounds = screen.getVisualBounds();
+	
+			primaryStage.setX(bounds.getMinX());
+			primaryStage.setY(bounds.getMinY());
+			primaryStage.setWidth(bounds.getWidth());
+			primaryStage.setHeight(bounds.getHeight());
+			
+	        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Main.fxml"));
+	        Parent root = loader.load();
+	        Scene scene = new Scene(root);
+			
+			primaryStage.setTitle("Facturación");
+			primaryStage.setScene(scene);
+	        primaryStage.show();
 		
-		primaryStage.setTitle("Facturación");
-		primaryStage.setScene(scene);
-        primaryStage.show();
-		
+		} catch(Exception e){
+			//log.error("Error al arrancar la aplicación", e);
+		}
 	}
 	
 	public static void main(String[] args) {
